ee43cafc21f4e28038a040bca902dac5f48f8d32
Ramsley Lichuma <113148758+RamsleyLichuma@users.noreply.github.com>
2024-02-23 08:34:26 +0300

    Update config.php

--- carousel/news/admin/includes/config.php
+++ carousel/news/admin/includes/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME','newsportal');
+define('DB_SERVER','mysql-main-db.cjoiw8u8kkbl.us-east-2.rds.amazonaws.com');
+define('DB_USER','mesa_user');
+define('DB_PASS' ,'buFk9YL764q7');
+define('DB_NAME','mesa');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
@@ -10,4 +10,3 @@ if (mysqli_connect_errno())
  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 ?>
- 
